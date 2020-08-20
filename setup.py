9922eedd472acf6f62e7abf29ef04abc2e057b41
LeonardoClemente23 <69744906+LeonardoClemente23@users.noreply.github.com>
2020-08-20 18:40:11 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setup(
             This is a sample python package for encapsulating custom
             tranforms from scikit-learn into Watson Machine Learning
       ''',
-      url='https://github.com/vnderlev/sklearn_transforms/',
-      author='Vanderlei Munhoz',
-      author_email='vnderlev@protonmail.ch',
+      url='https://github.com/LeonardoClemente23/sklearn_transforms/',
+      author='Leonardo Clemente',
+      author_email='leonardo.sosilva@gmail.com',
       license='BSD',
       packages=[
             'my_custom_sklearn_transforms'
